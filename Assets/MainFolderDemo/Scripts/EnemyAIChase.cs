f248f421be4e6cd338660e25df918b247fac1022
Diljot Sahota <diljotwarmnesquik@gmail.com>
2025-05-31 19:02:22 -0700

    ai chase

--- Assets/MainFolderDemo/Scripts/EnemyAIChase.cs
+++ Assets/MainFolderDemo/Scripts/EnemyAIChase.cs
@@ -3,29 +3,34 @@ using UnityEngine.AI;
 
 public class EnemyFollow : MonoBehaviour
 {
-    public NavMeshAgent navMeshAgent;
-    public GameObject playerTarget;
-
+    private NavMeshAgent agent;
+    public Transform target;
 
     void Start()
     {
-        if (!navMeshAgent.isOnNavMesh)
+        agent = GetComponent<NavMeshAgent>();
+
+        // Warp to NavMesh if not already placed
+        if (!agent.isOnNavMesh)
         {
             NavMeshHit hit;
             if (NavMesh.SamplePosition(transform.position, out hit, 5f, NavMesh.AllAreas))
             {
-                navMeshAgent.Warp(hit.position); // Snap to nearest navmesh point
+                agent.Warp(hit.position);
+                Debug.Log("Warped enemy to NavMesh.");
             }
             else
             {
-                Debug.LogError("Enemy is not near any NavMesh!");
+                Debug.LogError("Enemy is not near a valid NavMesh area!");
             }
         }
     }
 
     void Update()
     {
-        navMeshAgent.SetDestination(playerTarget.transform.position);
+        if (target != null && agent.isOnNavMesh)
+        {
+            agent.SetDestination(target.position);
+        }
     }
-
 }
